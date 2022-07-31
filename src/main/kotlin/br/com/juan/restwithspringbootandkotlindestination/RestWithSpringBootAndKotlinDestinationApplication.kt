9c71f3df37d0d7ef45d7208695439c6e80b49f8d
jminango20 <jcarlosminango@gmail.com>
2022-07-31 18:49:34 -0300

    first commit

---
+++ src/main/kotlin/br/com/juan/restwithspringbootandkotlindestination/RestWithSpringBootAndKotlinDestinationApplication.kt
@@ -0,0 +1,11 @@
+package br.com.juan.restwithspringbootandkotlindestination
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinDestinationApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinDestinationApplication>(*args)
+}
