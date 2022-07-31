8a1c126717d0e9d5c4211a4433fc7fa3b52fc1b5
jminango20 <jcarlosminango@gmail.com>
2022-07-31 19:02:59 -0300

    changing main name and importing db.migration

--- src/main/kotlin/br/com/juan/restwithspringbootandkotlindestination/Main.kt
+++ src/main/kotlin/br/com/juan/restwithspringbootandkotlindestination/Main.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RestWithSpringBootAndKotlinDestinationApplication
+class Main
 
 fun main(args: Array<String>) {
-	runApplication<RestWithSpringBootAndKotlinDestinationApplication>(*args)
+	runApplication<Main>(*args)
 }
